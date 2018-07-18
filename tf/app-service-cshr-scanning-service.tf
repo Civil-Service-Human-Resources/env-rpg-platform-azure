86048ef09bfc5f410cf63163f347d7b0d2a3ff29
Robert.Barnes <robert.raybarnes@gmail.com>
2018-07-18 16:35:12 +0100

    changed the scanning_service to scanning-service

--- tf/app-service-cshr-scanning-service.tf
+++ tf/app-service-cshr-scanning-service.tf
@@ -2,37 +2,39 @@
 
 ## Heavily borrowed from LPG XAPI - https://raw.githubusercontent.com/Civil-Service-Human-Resources/lpg-terraform-paas/master/modules/learning-locker-xapi/main.tf
 
-variable "application_name_scanning_service" {
+variable "application_name_scanning-service" {
   type    = "string"
-  default = "cshr-scanning_service"
+  default = "cshr-scanning-service"
 }
 
-resource "azurerm_resource_group" "rg_scanning_service" {
-  name     = "${var.global__service_id}-${var.environment__name}-rg-${var.application_name_scanning_service}"
+resource "azurerm_resource_group" "rg_scanning-service" {
+  name     = "${var.global__service_id}-${var.environment__name}-rg-${var.application_name_scanning-service}"
   location = "${var.global__region}"
 }
 
-data "template_file" "scanning_service_arm" {
+data "template_file" "scanning-service_arm" {
   template = "${file("${path.module}/templates/cshr-scanning-service.arm.template.json")}"
 
   vars {
-    app-service-cshr-scanning_service__certificatename  = "${var.app-service-cshr-scanning_service__certificatename}"
-    app-service-cshr-scanning_service__docker_image     = "${var.app-service-cshr-scanning_service__docker_image}"
-    app-service-cshr-scanning_service__docker_image_tag = "${var.app-service-cshr-scanning_service__docker_image_tag}"
+    app-service-cshr-scanning-service__certificatename  = "${var.app-service-cshr-scanning-service__certificatename}"
+    app-service-cshr-scanning-service__docker_image     = "${var.app-service-cshr-scanning-service__docker_image}"
+    app-service-cshr-scanning-service__docker_image_tag = "${var.app-service-cshr-scanning-service__docker_image_tag}"
 
-    app-service-cshr-scanning_service__ENC_docker_registry_password = "${var.app-service-cshr-clmav__ENC_docker_registry_password}"
-    app-service-cshr-scanning_service__ENC_docker_registry_user     = "${var.app-service-cshr-scanning_service__ENC_docker_registry_user}"
+    app-service-cshr-scanning-service__ENC_docker_registry_password = "${var.app-service-cshr-clmav__ENC_docker_registry_password}"
+    app-service-cshr-scanning-service__ENC_docker_registry_user     = "${var.app-service-cshr-scanning-service__ENC_docker_registry_user}"
+    app-service-cshr-scanning-service__FILEBEAT_ENVIRONMENT         = "${var.environment__name}"
+    app-service-cshr-scanning-service__ENC_FILEBEAT_HOSTS           = "${var.global__ENC_filebeat_hosts}"
 
-    app-service-cshr-scanning_service__CSHR_SCANNER_ENDPOINT            = "${var.app-service-cshr-scanning_service__CSHR_SCANNER_ENDPOINT}"
-    app-service-cshr-scanning_service__SERVER_PORT                      = "${var.app-service-cshr-scanning_service__SERVER_PORT}"
-    app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_PASSWORD = "${var.app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_PASSWORD}"
-    app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_USERNAME = "${var.app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_USERNAME}"
+    app-service-cshr-scanning-service__CSHR_SCANNER_ENDPOINT            = "${var.app-service-cshr-scanning-service__CSHR_SCANNER_ENDPOINT}"
+    app-service-cshr-scanning-service__SERVER_PORT                      = "${var.app-service-cshr-scanning-service__SERVER_PORT}"
+    app-service-cshr-scanning-service__SPRING_SECURITY_SERVICE_PASSWORD = "${var.app-service-cshr-scanning-service__ENC_SPRING_SECURITY_SERVICE_PASSWORD}"
+    app-service-cshr-scanning-service__SPRING_SECURITY_SERVICE_USERNAME = "${var.app-service-cshr-scanning-service__ENC_SPRING_SECURITY_SERVICE_USERNAME}"
 
-    app-service-cshr-scanning_service__existingkeyvaultsecretname = "${var.app-service-cshr-scanning_service__existingkeyvaultsecretname}"
-    app-service-cshr-scanning_service__vaultname                  = "${var.app-service-cshr-scanning_service__vaultname}"
-    app-service-cshr-scanning_service__vaultresourcegroup         = "${var.app-service-cshr-scanning_service__vaultresourcegroup}"
+    app-service-cshr-scanning-service__existingkeyvaultsecretname = "${var.app-service-cshr-scanning-service__existingkeyvaultsecretname}"
+    app-service-cshr-scanning-service__vaultname                  = "${var.app-service-cshr-scanning-service__vaultname}"
+    app-service-cshr-scanning-service__vaultresourcegroup         = "${var.app-service-cshr-scanning-service__vaultresourcegroup}"
     application_name                                              = "${var.application_name_api}"
-    app-service-cshr-scanning_service__WEBSITES_PORT              = "${var.app-service-cshr-scanning_service__WEBSITES_PORT}"
+    app-service-cshr-scanning-service__WEBSITES_PORT              = "${var.app-service-cshr-scanning-service__WEBSITES_PORT}"
     environment__name                                             = "${var.environment__name}"
     global__deployed_by                                           = "${var.global__deployed_by}"
     global__environment_commit                                    = "${var.global__environment_commit}"
@@ -42,12 +44,12 @@ data "template_file" "scanning_service_arm" {
   }
 }
 
-resource "azurerm_template_deployment" "cshr_scanning_service" {
-  name                = "${var.global__service_id}-${var.environment__name}-${var.application_name_scanning_service}"
-  resource_group_name = "${azurerm_resource_group.rg_scanning_service.name}"
+resource "azurerm_template_deployment" "cshr_scanning-service" {
+  name                = "${var.global__service_id}-${var.environment__name}-${var.application_name_scanning-service}"
+  resource_group_name = "${azurerm_resource_group.rg_scanning-service.name}"
 
-  template_body = "${data.template_file.scanning_service_arm.rendered}"
+  template_body = "${data.template_file.scanning-service_arm.rendered}"
 
   deployment_mode = "Incremental"
-  depends_on      = ["azurerm_resource_group.rg_scanning_service"]
+  depends_on      = ["azurerm_resource_group.rg_scanning-service"]
 }
--- tf/app-service-cshr-scanning-service__variables.tf
+++ tf/app-service-cshr-scanning-service__variables.tf
@@ -1,54 +1,55 @@
-variable "app-service-cshr-scanning_service__docker_image" {
+variable "app-service-cshr-scanning-service__docker_image" {
   type    = "string"
-  default = "quay.io/ukhomeofficedigital/scanning_service"
+  default = "cshrrpg.azurecr.io/cshr-scanning-service"
 }
 
-variable "app-service-cshr-scanning_service__docker_image_tag" {
+variable "app-service-cshr-scanning-service__docker_image_tag" {
   type = "string"
 }
 
-variable "app-service-cshr-scanning_service__ENC_docker_registry_user" {
+variable "app-service-cshr-scanning-service__ENC_docker_registry_user" {
   type        = "string"
   description = "the ACR user name to pull the image"
 }
 
-variable "app-service-cshr-scanning_service__ENC_docker_registry_password" {
+variable "app-service-cshr-scanning-service__ENC_docker_registry_password" {
   type        = "string"
   description = "the ACR password to pull the image"
 }
 
-variable "app-service-cshr-scanning_service__vaultresourcegroup" {
+variable "app-service-cshr-scanning-service__vaultresourcegroup" {
   default = "lpg-prod-keyvault"
 }
 
-variable "app-service-cshr-scanning_service__vaultname" {
+variable "app-service-cshr-scanning-service__vaultname" {
   default = "lpg-prod-kv"
 }
 
-variable "app-service-cshr-scanning_service__existingkeyvaultsecretname" {
+variable "app-service-cshr-scanning-service__existingkeyvaultsecretname" {
   default = "starcshrdigital-pfxsecret"
 }
 
-variable "app-service-cshr-scanning_service__certificatename" {
+variable "app-service-cshr-scanning-service__certificatename" {
   default = "starcshrdigital"
 }
 
-variable "app-service-cshr-scanning_service__CSHR_SCANNER_ENDPOINT" {
+variable "app-service-cshr-scanning-service__CSHR_SCANNER_ENDPOINT" {
   type        = "string"
   description = "clamav end point"
 }
 
-variable "app-service-cshr-scanning_service__SERVER_PORT" {
+variable "app-service-cshr-scanning-service__SERVER_PORT" {
   type        = "string"
   description = ""
+  default     = "443"
 }
 
-variable "app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_PASSWORD" {
+variable "app-service-cshr-scanning-service__ENC_SPRING_SECURITY_SERVICE_PASSWORD" {
   type        = "string"
   description = "spring-boot password"
 }
 
-variable "app-service-cshr-scanning_service__SPRING_SECURITY_SERVICE_USERNAME" {
+variable "app-service-cshr-scanning-service__ENC_SPRING_SECURITY_SERVICE_USERNAME" {
   type        = "string"
   description = "spring-boot username"
 }
